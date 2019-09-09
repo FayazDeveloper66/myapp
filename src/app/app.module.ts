03926f90ab18ac815a00b21f0469f2b675eb965e
fayaz-furious <fayazmd6644@gmail.com>
2019-09-09 17:00:28 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 
+import { AppComponent } from './app.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+import { MoviealbumsComponent } from './moviealbums/moviealbums.component';
+import { FavoritealbumsComponent } from './favoritealbums/favoritealbums.component';
+const routes: Routes = [
+  { path: '', component: SignupComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'moviealbums', component: MoviealbumsComponent },
+  { path: 'favoritealbums', component: FavoritealbumsComponent }
+  
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    LoginComponent,
+    MoviealbumsComponent,
+    FavoritealbumsComponent
   ],
+  
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule, BrowserModule, FormsModule, BrowserAnimationsModule, HttpClientModule,AppRoutingModule,
+   
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
